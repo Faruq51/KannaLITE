d88ed43f3f51aea600bf83465e7dc6302f678966
FARUQOFC <113996402+Faruq51@users.noreply.github.com>
2022-11-01 20:37:19 +0700

    Update tiktok.js

--- plugins/download/tiktok.js
+++ plugins/download/tiktok.js
@@ -11,32 +11,32 @@ exports.run = {
       try {
          if (!args || !args[0]) return client.reply(m.chat, Func.example(isPrefix, command, 'https://vt.tiktok.com/ZSe22y3dA'), m)
          if (!args[0].match('tiktok.com')) return client.reply(m.chat, global.status.invalid, m)
-         client.sendReact(m.chat, '🕒', m.key)
+         client.reply(m.chat, global.status.getdata, m)
          let old = new Date()
          let json = await Api.tiktok(Func.ttFixed(args[0]))
          if (!json.status) return client.reply(m.chat, Func.jsonFormat(json), m)
-         if (command == 'tiktok') return client.sendButton(m.chat, json.data.video, `Jika Anda ingin mendapatkan *suara asli* tekan tombol di bawah.\n🍟 *Mengambil* : ${((new Date - old) * 1)} ms`, ``, m, [{
+         if (command == 'tiktok') return client.sendButton(m.chat, json.data.video, `dari : ${json.author}\n${json.caption}\n\n*Mengambil* : ${((new Date - old) * 1)} detik`, ``, m, [{
+            buttonId: `${isPrefix}extract ${json.data.video}`,
+            buttonText: {
+               displayText: 'Audio'
+            },
+            type: 1
+         }])
+         if (command == 'twm') return client.sendButton(m.chat, json.data.videoWM, `*Mengambil* : ${((new Date - old) * 1)} detik`, ``, m, [{
             buttonId: `${isPrefix}tikmp3 ${args[0]}`,
             buttonText: {
                displayText: 'Audio'
             },
             type: 1
          }])
-         if (command == 'tikwm') return client.sendButton(m.chat, json.data.videoWM, `Jika Anda ingin mendapatkan *suara asli* tekan tombol di bawah.\n🍟 *Mengambil* : ${((new Date - old) * 1)} ms`, ``, m, [{
-            buttonId: `${isPrefix}tikmp3 ${args[0]}`,
-            buttonText: {
-               displayText: 'Audio'
-            },
-            type: 1
-         }])
-         if (command == 'tikmp3') return !json.data.audio ? client.reply(m.chat, global.status.fail, m) : client.sendFile(m.chat, json.data.audio, 'audio.mp3', '', m)
+         if (command == 'tmp3') return !json.data.audio ? client.reply(m.chat, global.status.fail, m) : client.sendFile(m.chat, json.data.audio, 'audio.mp3', '', m)
       } catch (e) {
          console.log(e)
          return client.reply(m.chat, global.status.error, m)
       }
    },
    error: false,
-   limit: true,
+   limit: false,
    cache: true,
    location: __filename
 }

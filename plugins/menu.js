6e4b103802470fd33e519d1ae0c607bb804e7fbf
FARUQOFC <113996402+Faruq51@users.noreply.github.com>
2022-11-02 16:33:40 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -7,182 +7,175 @@ exports.run = {
       isPrefix
    }) => {
       try {
-      let captions = `     🔮  *I N F O - B O T*  
+      let captions = `╭────❏ *INFO BOT* ❏
+│⭔  Sistem otomatis (WhatsApp Bot) yang dapat membantu untuk melakukan sesuatu,
+│⭔  mencari dan mendapatkan data/informasi hanya melalui WhatsApp.
+│⭔  Gunakan bot di private message agar bot merespon dengan cepat
+│⭔  Beri jeda  yah kak ｡⁠◕⁠‿⁠◕⁠｡
+╰────────────────────❏
 
-🚥  Sistem otomatis (WhatsApp Bot) yang dapat membantu untuk melakukan sesuatu,
-🚥  mencari dan mendapatkan data/informasi hanya melalui WhatsApp.
-🚥  Gunakan bot di private message agar bot merespon dengan cepat
-🚥  Beri jeda  yah kak ｡⁠◕⁠‿⁠◕⁠｡
+╭────❏ *INFO CMD* ❏
+│owner 🅟
+│⭔ Free user 🅕
+╰───────────❏
 
-     🔮  *I N F O R M A S I*  
-        
-🚥 Cpecial owner 🅟
-🚥 Free user 🅕
+╭────❏ *CEK USER* ❏
+│⭔  ${isPrefix}limit 🅕
+│⭔  ${isPrefix}me 🅕
+╰─────────────❏
 
-     🔮  *U S E R - I N F O*  
-      
-🚥  ${isPrefix}limit 🅕
-🚥  ${isPrefix}me 🅕
+╭────❏ *GROUP MENU* ❏
+│⭔  ${isPrefix}antidelete *on / off* 🅕
+│⭔  ${isPrefix}antilink *on / off* 🅕
+│⭔  ${isPrefix}antivirtex *on / off* 🅕
+│⭔  ${isPrefix}demote *mention or reply* 🅕
+│⭔  ${isPrefix}everyone *text (optional)* 🅕
+│⭔  ${isPrefix}filter *on / off* 🅕
+│⭔  ${isPrefix}group *open / close* 🅕
+│⭔  ${isPrefix}hidetag *text* 🅕
+│⭔  ${isPrefix}kick *mention or reply* 🅕
+│⭔  ${isPrefix}left *on / off* 🅕
+│⭔  ${isPrefix}localonly *on / off* 🅕
+│⭔  ${isPrefix}mute *0 / 1* 🅕
+│⭔  ${isPrefix}setdesc *text* 🅕
+│⭔  ${isPrefix}setleft *text* 🅟
+│⭔  ${isPrefix}setname *text* 🅟
+│⭔  ${isPrefix}setout *text* 🅕
+│⭔  ${isPrefix}setwelcome *text* 🅕
+│⭔  ${isPrefix}welcome *on / off* 🅕
+│⭔  ${isPrefix}afk *reason (optional)* 🅕
+│⭔  ${isPrefix}ava *mention or reply* 🅕
+│⭔  ${isPrefix}delete *reply chat* 🅟
+│⭔  ${isPrefix}q *reply chat* 🅟
+╰─────────────────────❏
 
-     🔮  *G R O U P*  
+╭────❏ *CONVERT MENU* ❏
+│⭔  ${isPrefix}emojimix *emoji + emoji* 🅕
+│⭔  ${isPrefix}sticker *reply media* 🅕
+│⭔  ${isPrefix}swm *packname | author* 🅕
+│⭔  ${isPrefix}take *packname | author* 🅕
+│⭔  ${isPrefix}toimg *reply sticker* 🅕
+│⭔  ${isPrefix}tomp3 *reply media* 🅕
+│⭔  ${isPrefix}tovn *reply media* 🅕
+╰─────────────────────────❏
 
-🚥  ${isPrefix}afk *reason (optional)* 🅕
-🚥  ${isPrefix}ava *mention or reply* 🅕
-🚥  ${isPrefix}delete *reply chat* 🅟
-🚥  ${isPrefix}q *reply chat* 🅟
+╭────❏ *DOWNLOAD MENU* ❏
+│⭔  ${isPrefix}apk *query* 🅕
+│⭔  ${isPrefix}fb *link* 🅕
+│⭔  ${isPrefix}ig *link* 🅕
+│⭔  ${isPrefix}igs *username / link* 🅕
+│⭔  ${isPrefix}mediafire *link* 🅕
+│⭔  ${isPrefix}pin *link* 🅕
+│⭔  ${isPrefix}play *query* 🅕
+│⭔  ${isPrefix}podcast *link* 🅕
+│⭔  ${isPrefix}soundcloud *query / link* 🅕
+│⭔  ${isPrefix}tikmp3 *link* 🅕
+│⭔  ${isPrefix}tiktok *link* 🅕
+│⭔  ${isPrefix}tikwm *link* 🅕
+│⭔  ${isPrefix}twitter *link* 🅕
+│⭔  ${isPrefix}video *query* 🅕
+│⭔  ${isPrefix}ytmp3 *link* 🅕
+│⭔  ${isPrefix}ytmp4 *link* 🅕
+╰─────────────────────❏
 
-     🔮  *C O N V E R T E R*  
+╭────❏ *SEARCH MENU* ❏
+│⭔  ${isPrefix}brainly *question* 🅕
+│⭔  ${isPrefix}halah *text or reply chat* 🅕
+│⭔  ${isPrefix}heleh *text or reply chat* 🅕
+│⭔  ${isPrefix}hilih *text or reply chat* 🅕
+│⭔  ${isPrefix}holoh *text or reply chat* 🅕
+│⭔  ${isPrefix}huluh *text or reply chat* 🅕
+│⭔  ${isPrefix}pinterest *query* 🅕
+│⭔  ${isPrefix}response *link* 🅕
+│⭔  ${isPrefix}server 🅕
+│⭔  ${isPrefix}wallpaper *query* 🅕
+╰────────────────────────❏
 
-🚥  ${isPrefix}emojimix *emoji + emoji* 🅕
-🚥  ${isPrefix}sticker *reply media* 🅕
-🚥  ${isPrefix}swm *packname | author* 🅕
-🚥  ${isPrefix}take *packname | author* 🅕
-🚥  ${isPrefix}toimg *reply sticker* 🅕
-🚥  ${isPrefix}tomp3 *reply media* 🅕
-🚥  ${isPrefix}tovn *reply media* 🅕
+╭────❏ *TEXT MAKER* ❏
+│⭔  ${isPrefix}blackpink *text* 🅕
+│⭔  ${isPrefix}blood *text* 🅕
+│⭔  ${isPrefix}breakwall *text* 🅕
+│⭔  ${isPrefix}glow *text* 🅕
+│⭔  ${isPrefix}joker *text* 🅕
+│⭔  ${isPrefix}magma *text* 🅕
+│⭔  ${isPrefix}matrix *text* 🅕
+│⭔  ${isPrefix}multicolor *text* 🅕
+│⭔  ${isPrefix}neon *text* 🅕
+│⭔  ${isPrefix}papercut *text* 🅕
+│⭔  ${isPrefix}slice *text* 🅕
+│⭔  ${isPrefix}alien *reply foto* 🅕
+│⭔  ${isPrefix}brick *reply foto* 🅕
+│⭔  ${isPrefix}bunny *reply foto* 🅕
+│⭔  ${isPrefix}caricature *reply foto* 🅕
+│⭔  ${isPrefix}clown *reply foto* 🅕
+│⭔  ${isPrefix}ink *reply foto* 🅕
+│⭔  ${isPrefix}latte *reply foto* 🅕
+│⭔  ${isPrefix}letter *reply foto* 🅕
+│⭔  ${isPrefix}pencil *reply foto* 🅕
+│⭔  ${isPrefix}puzzle *reply foto* 🅕
+│⭔  ${isPrefix}roses *reply foto* 🅕
+│⭔  ${isPrefix}sketch *reply foto* 🅕 
+│⭔  ${isPrefix}splash *reply foto* 🅕
+│⭔  ${isPrefix}staco *reply foto* 🅕
+╰────────────────────────❏
 
-     🔮  *D O W N L O A D E R*  
+╭────❏ *OWNER MENU* ❏
+│⭔  ${isPrefix}-cmdstic *text / command* 🅟
+│⭔  ${isPrefix}-mimic *mention or reply* 🅟
+│⭔  ${isPrefix}-owner *mention or reply* 🅟
+│⭔  ${isPrefix}-prefix *symbol* 🅟
+│⭔  ${isPrefix}-prem *mention or reply* 🅟
+│⭔  ${isPrefix}+cmdstic *text / command* 🅟
+│⭔  ${isPrefix}+mimic *mention or reply* 🅟
+│⭔  ${isPrefix}+owner *mention or reply* 🅟
+│⭔  ${isPrefix}+prefix *symbol* 🅟
+│⭔  ${isPrefix}+prem *mention or reply* 🅟
+│⭔  ${isPrefix}autodownload *on / off* 🅟
+│⭔  ${isPrefix}autoread *on / off* 🅟
+│⭔  ${isPrefix}backup 🅟
+│⭔  ${isPrefix}ban *mention or reply* 🅟
+│⭔  ${isPrefix}bc *text or reply media* 🅟
+│⭔  ${isPrefix}bcgc *text or reply media* 🅟
+│⭔  ${isPrefix}block *mention or reply* 🅟
+│⭔  ${isPrefix}changename *text* 🅟
+│⭔  ${isPrefix}chatbot *on / off* 🅟
+│⭔  ${isPrefix}cmdstic 🅟
+│⭔  ${isPrefix}debug *on / off* 🅟
+│⭔  ${isPrefix}groupmode *on / off* 🅟
+│⭔  ${isPrefix}join *group link* 🅟
+│⭔  ${isPrefix}multiprefix *on / off* 🅟
+│⭔  ${isPrefix}ohidetag *text* 🅟
+│⭔  ${isPrefix}oleft *on / off* 🅟
+│⭔  ${isPrefix}omute *0 / 1* 🅟
+│⭔  ${isPrefix}online *on / off* 🅟
+│⭔  ${isPrefix}owelcome *on / off* 🅟
+│⭔  ${isPrefix}plugdis *plugin name* 🅟
+│⭔  ${isPrefix}plugen *plugin name* 🅟
+│⭔  ${isPrefix}prefix *symbol* 🅟
+│⭔  ${isPrefix}reset 🅟
+│⭔  ${isPrefix}restart 🅟
+│⭔  ${isPrefix}self *on / off* 🅟
+│⭔  ${isPrefix}setmsg *text* 🅟
+│⭔  ${isPrefix}setpp *reply photo* 🅟
+│⭔  ${isPrefix}setwm *packname | author* 🅟
+│⭔  ${isPrefix}unban *mention or reply* 🅟
+│⭔  ${isPrefix}unblock *mention or reply* 🅟
+│⭔  ${isPrefix}update 🅟
+╰─────────────────❏
 
-🚥  ${isPrefix}apk *query* 🅕
-🚥  ${isPrefix}fb *link* 🅕
-🚥  ${isPrefix}ig *link* 🅕
-🚥  ${isPrefix}igs *username / link* 🅕
-🚥  ${isPrefix}mediafire *link* 🅕
-🚥  ${isPrefix}pin *link* 🅕
-🚥  ${isPrefix}play *query* 🅕
-🚥  ${isPrefix}podcast *link* 🅕
-🚥  ${isPrefix}soundcloud *query / link* 🅕
-🚥  ${isPrefix}tikmp3 *link* 🅕
-🚥  ${isPrefix}tiktok *link* 🅕
-🚥  ${isPrefix}tikwm *link* 🅕
-🚥  ${isPrefix}twitter *link* 🅕
-🚥  ${isPrefix}video *query* 🅕
-🚥  ${isPrefix}ytmp3 *link* 🅕
-🚥  ${isPrefix}ytmp4 *link* 🅕
+╭────❏ *SPESIAL OWNER* ❏
+│⭔  ${isPrefix}botstat 
+│⭔  ${isPrefix}checkapi 
+│⭔  ${isPrefix}groups 
+│⭔  ${isPrefix}hitdaily 
+│⭔  ${isPrefix}hitstat 
+│⭔  ${isPrefix}list 
+│⭔  ${isPrefix}owner 
+│⭔  ${isPrefix}premium 
+│⭔  ${isPrefix}runtime
+╰───────────────❏
 
-     🔮  *S E A R C H*  
-
-🚥  ${isPrefix}brainly *question* 🅕
-🚥  ${isPrefix}halah *text or reply chat* 🅕
-🚥  ${isPrefix}heleh *text or reply chat* 🅕
-🚥  ${isPrefix}hilih *text or reply chat* 🅕
-🚥  ${isPrefix}holoh *text or reply chat* 🅕
-🚥  ${isPrefix}huluh *text or reply chat* 🅕
-🚥  ${isPrefix}pinterest *query* 🅕
-🚥  ${isPrefix}response *link* 🅕
-🚥  ${isPrefix}server 🅕
-🚥  ${isPrefix}wallpaper *query* 🅕
-
-     🔮  *T E X T - M A K E R*  
-
-🚥  ${isPrefix}blackpink *text* 🅕
-🚥  ${isPrefix}blood *text* 🅕
-🚥  ${isPrefix}breakwall *text* 🅕
-🚥  ${isPrefix}glow *text* 🅕
-🚥  ${isPrefix}joker *text* 🅕
-🚥  ${isPrefix}magma *text* 🅕
-🚥  ${isPrefix}matrix *text* 🅕
-🚥  ${isPrefix}multicolor *text* 🅕
-🚥  ${isPrefix}neon *text* 🅕
-🚥  ${isPrefix}papercut *text* 🅕
-🚥  ${isPrefix}slice *text* 🅕
-
-     🔮 *I M A G E - E F F E C T*  
-
-🚥  ${isPrefix}alien *reply foto* 🅕
-🚥  ${isPrefix}brick *reply foto* 🅕
-🚥  ${isPrefix}bunny *reply foto* 🅕
-🚥  ${isPrefix}caricature *reply foto* 🅕
-🚥  ${isPrefix}clown *reply foto* 🅕
-🚥  ${isPrefix}ink *reply foto* 🅕
-🚥  ${isPrefix}latte *reply foto* 🅕
-🚥  ${isPrefix}letter *reply foto* 🅕
-🚥  ${isPrefix}pencil *reply foto* 🅕
-🚥  ${isPrefix}puzzle *reply foto* 🅕
-🚥  ${isPrefix}roses *reply foto* 🅕
-🚥  ${isPrefix}sketch *reply foto* 🅕 
-🚥  ${isPrefix}splash *reply foto* 🅕
-🚥  ${isPrefix}staco *reply foto* 🅕
-
-     🔮  *A D M I N - T O O L S*  
-
-🚥  ${isPrefix}antidelete *on / off* 🅕
-🚥  ${isPrefix}antilink *on / off* 🅕
-🚥  ${isPrefix}antivirtex *on / off* 🅕
-🚥  ${isPrefix}demote *mention or reply* 🅕
-🚥  ${isPrefix}everyone *text (optional)* 🅕
-🚥  ${isPrefix}filter *on / off* 🅕
-🚥  ${isPrefix}group *open / close* 🅕
-🚥  ${isPrefix}hidetag *text* 🅕
-🚥  ${isPrefix}kick *mention or reply* 🅕
-🚥  ${isPrefix}left *on / off* 🅕
-🚥  ${isPrefix}localonly *on / off* 🅕
-🚥  ${isPrefix}mute *0 / 1* 🅕
-🚥  ${isPrefix}setdesc *text* 🅕
-🚥  ${isPrefix}setleft *text* 🅟
-🚥  ${isPrefix}setname *text* 🅟
-🚥  ${isPrefix}setout *text* 🅕
-🚥  ${isPrefix}setwelcome *text* 🅕
-🚥  ${isPrefix}welcome *on / off* 🅕
-
-     🔮  *O W N E R*  
-
-🚥  ${isPrefix}-cmdstic *text / command* 🅟
-🚥  ${isPrefix}-mimic *mention or reply* 🅟
-🚥  ${isPrefix}-owner *mention or reply* 🅟
-🚥  ${isPrefix}-prefix *symbol* 🅟
-🚥  ${isPrefix}-prem *mention or reply* 🅟
-🚥  ${isPrefix}+cmdstic *text / command* 🅟
-🚥  ${isPrefix}+mimic *mention or reply* 🅟
-🚥  ${isPrefix}+owner *mention or reply* 🅟
-🚥  ${isPrefix}+prefix *symbol* 🅟
-🚥  ${isPrefix}+prem *mention or reply* 🅟
-🚥  ${isPrefix}autodownload *on / off* 🅟
-🚥  ${isPrefix}autoread *on / off* 🅟
-🚥  ${isPrefix}backup 🅟
-🚥  ${isPrefix}ban *mention or reply* 🅟
-🚥  ${isPrefix}bc *text or reply media* 🅟
-🚥  ${isPrefix}bcgc *text or reply media* 🅟
-🚥  ${isPrefix}block *mention or reply* 🅟
-🚥  ${isPrefix}changename *text* 🅟
-🚥  ${isPrefix}chatbot *on / off* 🅟
-🚥  ${isPrefix}cmdstic 🅟
-🚥  ${isPrefix}debug *on / off* 🅟
-🚥  ${isPrefix}groupmode *on / off* 🅟
-🚥  ${isPrefix}join *group link* 🅟
-🚥  ${isPrefix}multiprefix *on / off* 🅟
-🚥  ${isPrefix}ohidetag *text* 🅟
-🚥  ${isPrefix}oleft *on / off* 🅟
-🚥  ${isPrefix}omute *0 / 1* 🅟
-🚥  ${isPrefix}online *on / off* 🅟
-🚥  ${isPrefix}owelcome *on / off* 🅟
-🚥  ${isPrefix}plugdis *plugin name* 🅟
-🚥  ${isPrefix}plugen *plugin name* 🅟
-🚥  ${isPrefix}prefix *symbol* 🅟
-🚥  ${isPrefix}reset 🅟
-🚥  ${isPrefix}restart 🅟
-🚥  ${isPrefix}self *on / off* 🅟
-🚥  ${isPrefix}setmsg *text* 🅟
-🚥  ${isPrefix}setpp *reply photo* 🅟
-🚥  ${isPrefix}setwm *packname | author* 🅟
-🚥  ${isPrefix}unban *mention or reply* 🅟
-🚥  ${isPrefix}unblock *mention or reply* 🅟
-🚥  ${isPrefix}update 🅟
-
-     🔮  *S P E C I A L*  
-
-🚥  ${isPrefix}botstat 🅟
-🚥  ${isPrefix}checkapi 🅟
-🚥  ${isPrefix}groups 🅟
-🚥  ${isPrefix}hitdaily 🅟
-🚥  ${isPrefix}hitstat 🅟
-🚥  ${isPrefix}list 🅟
-🚥  ${isPrefix}owner 🅟
-🚥  ${isPrefix}premium 🅟
-🚥  ${isPrefix}runtime🅟
-
-    
-    © Powered by Faruqofc`
+© Powered by Faruqofc`
       client.sendMessageModify(m.chat, captions, m, {
                title: '𝑲𝑨𝑵𝑵𝑨 𝑽 2.6.4',
                largeThumb: true,
